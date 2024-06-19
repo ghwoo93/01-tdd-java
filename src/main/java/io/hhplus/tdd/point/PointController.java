3facfcd38229bd43fd0e6a218ac29ff18ccbb13d
BOOK-BBIGU\Administrator <wkungh93@gmail.com>
2024-06-19 15:44:54 +0900

    pr 용 베이스 푸쉬

--- src/main/java/io/hhplus/tdd/point/PointController.java
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -2,11 +2,7 @@ package io.hhplus.tdd.point;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
-import io.hhplus.tdd.ErrorResponse;
 
 import java.util.List;
 
@@ -15,21 +11,15 @@ import java.util.List;
 public class PointController {
 
     private static final Logger log = LoggerFactory.getLogger(PointController.class);
-    private final PointService pointService;
-
-    @Autowired
-    public PointController(PointService pointService) {
-        this.pointService = pointService;
-    }
 
     /**
      * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
      */
     @GetMapping("{id}")
     public UserPoint point(
-            @PathVariable("id") long id
+            @PathVariable long id
     ) {
-        return pointService.getUserPoint(id);
+        return new UserPoint(0, 0, 0);
     }
 
     /**
@@ -37,11 +27,9 @@ public class PointController {
      */
     @GetMapping("{id}/histories")
     public List<PointHistory> history(
-            @PathVariable("id") long id
+            @PathVariable long id
     ) {
-        //return List.of();
-        //log.info("findPointHistoriesByUserId: {}", id);
-        return pointService.findPointHistoriesByUserId(id);
+        return List.of();
     }
 
     /**
@@ -49,11 +37,10 @@ public class PointController {
      */
     @PatchMapping("{id}/charge")
     public UserPoint charge(
-            @PathVariable("id") long id,
+            @PathVariable long id,
             @RequestBody long amount
     ) {
-        //return new UserPoint(0, 0, 0);
-        return pointService.updatePointAndHistory(id, amount, TransactionType.CHARGE);
+        return new UserPoint(0, 0, 0);
     }
 
     /**
@@ -61,17 +48,9 @@ public class PointController {
      */
     @PatchMapping("{id}/use")
     public UserPoint use(
-            @PathVariable("id") long id,
+            @PathVariable long id,
             @RequestBody long amount
     ) {
-        //return new UserPoint(0, 0, 0);
-        return pointService.updatePointAndHistory(id, amount, TransactionType.USE);
-    }
-
-    @ExceptionHandler(IllegalArgumentException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleIllegalArgumentException(IllegalArgumentException ex) {
-        log.error("IllegalArgumentException: {}", ex.getMessage());
-        return new ErrorResponse("400", ex.getMessage());
+        return new UserPoint(0, 0, 0);
     }
 }
